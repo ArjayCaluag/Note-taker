126ee035e0c986f82bbbf6d68f913ca8f8c7967b
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-26 20:45:15 -0800

    Create app.delete path with id parameter

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -25,6 +25,7 @@ module.exports = function (app) {
             // Create unique ids for each note
             noteInfo.forEach( function(note, id){
             note.id = id + 1});
+            console.log(noteInfo);
 
             // stringifys new notes and adds onto the db.json file.
             fs.writeFile('db/db.json', JSON.stringify(noteInfo), 'utf8', function (err) {
@@ -33,5 +34,30 @@ module.exports = function (app) {
         })
 
         res.json(notes)
+    });
+
+    // Path with the parameter id route
+    app.delete("/api/notes/:id", function (req, res) {
+        var emptyNote = req.params.id
+        
+
+        fs.readFile("db/db.json", "utf8", function (err,data) {
+            if (err) throw err
+
+            var noteDelete = JSON.parse(data)
+            // Takes id of note and - 1 to get index location
+            var index = parseInt(emptyNote) - 1
+            // removes or replaces 1 element at index location
+            noteDelete.splice(index, 1);
+            console.log("Note at index of " + index + " deleted.");
+            console.log(noteDelete);
+            
+            // rewrite db.json file once notes are deleted
+            fs.writeFile("db/db.json", JSON.stringify(noteDelete), "utf8", function(err){
+                if (err) throw err
+            })
+        })
+        res.send(emptyNote)
     })
+
 };
