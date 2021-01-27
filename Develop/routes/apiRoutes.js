ba7f2ca691673acbf41fa855c90bfd1c6fa89550
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-26 16:13:32 -0800

    Create POST api route to post new notes onto json file and server

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,11 +3,32 @@ const fs = require("fs")
 
 // Gives access to our express server (app)
 // Reads the file in db.json and returns the current hard coded notes in json data
-module.exports = function(app){
-app.get("/api/notes", function(req,res) {
-    fs.readFile("db/db.json", "utf8", function (err, data) {
-        if (err) throw err
-        var notes = JSON.parse(data);
-        res.json(notes);
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile("db/db.json", "utf8", function (err, data) {
+            if (err) throw err
+            var notes = JSON.parse(data);
+            res.json(notes);
+        })
+    });
+
+    // app.post method to handle POST request to the body.
+    app.post("/api/notes", function (req, res) {
+        var notes = req.body
+
+        // Read db/db.json file and pushes new notes to server
+        fs.readFile("db/db.json", "utf8", function (err, data) {
+            if (err) throw err
+            var noteInfo = JSON.parse(data)
+
+            noteInfo.push(notes)
+
+            // stringifys new notes and adds onto the db.json file.
+            fs.writeFile('db/db.json', JSON.stringify(noteInfo), 'utf8', function (err) {
+                if (err) throw err
+            })
+        })
+
+        res.json(notes)
     })
-})};
+};
